0608a070d171965673d041ce9e5bf003fdd78bca
KevinFerreira-BYUI <kferreira@byupathway.edu>
2025-05-29 16:56:37 -0300

    sup

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -8,3 +8,15 @@ toggleBtn.addEventListener("click", () => {
   menuNav.classList.toggle("open");
   toggleBtn.classList.toggle("open");
 });
+
+const nonLevelModal = document.getElementById("nonLevelModal");
+
+function displayModals(){
+  nonLevelModal.innerHTML = "";
+  nonLevelModal.innerHTML = `
+    <button class="closeModal">✖</button>
+    <h1>Ideal pra pequenos negócios que estão começando ou querem só marcar presença.</h1>
+    <h2></h2>
+
+  `;
+}
